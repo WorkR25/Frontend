16205959ff00357f09823aa5037ccd35d47fc078
Pratham <sinhapratham1234@gmail.com>
2025-09-11 15:20:43 +0530

    #fix: wrapped useSearchParams in Suspence

--- src/app/signup/page.tsx
+++ src/app/signup/page.tsx
@@ -1,13 +1,23 @@
 // 'use client'
 import SignupForm from "@/components/signup/SignUpForm";
 import SignUpHero from "@/components/signup/SignUpHero";
+import TripleDotLoader from "@/components/TripleDotLoader";
+import { Suspense } from "react";
 
 export default function Page() {
   return (
     <div className="text-black bg-white font-poppins h-full w-full">
       <div className="flex w-screen h-[100%] p-2 gap-x-2">
         <div className="sm:basis-45/100 w-full sm:w-auto flex items-center justify-center rounded-2xl shadow-xs shadow-black overflow-y-scroll">
-          <SignupForm/>
+          <Suspense
+            fallback={
+              <div>
+                <TripleDotLoader />
+              </div>
+            }
+          >
+            <SignupForm />
+          </Suspense>
         </div>
         <div className="bg-[#E7F2F8] basis-55/100  sm:flex hidden items-center justify-center shadow-xs shadow-black rounded-2xl overflow-hidden">
           <SignUpHero />
@@ -16,5 +26,3 @@ export default function Page() {
     </div>
   );
 }
-
-
